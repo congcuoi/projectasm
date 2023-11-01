f1721403d6b2cfe2c687d38ac819123791a840a2
Tran Quoc Thang <quocthangtran0@gmail.com>
2023-11-01 09:51:49 +0700

    change css QuocThang

---
+++ view/assets/javascript/sidebar.js
@@ -0,0 +1,9 @@
+// var a = document.getElementsByClassName("rotate");
+
+// a.addEventListener("click", function () {
+//   alert("Hello");
+// });
+
+// document.getElementById("rotate").addEventListener("click", function () {
+//   alert("Hello");
+// });
